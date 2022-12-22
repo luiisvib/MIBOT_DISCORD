f98cd091c53af94f7d2882ea8945febb784b68d2
Luis Iñigo <luisvib2003@gmail.com>
2022-12-22 09:16:18 +0100

    segundo

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { DISCORD_TOKEN, CLIENT_ID } = require("./config")
 const { Client, GatewayIntentBits } = require("discord.js")
 const colors = require("colors")
 const { default: test } = require("node:test")
+const { channel } = require("diagnostics_channel")
 
 const cliente = new Client({
     intents: [
@@ -24,12 +25,60 @@ cliente.once("ready", () => {  //Con el on siempre esta escuchando peticiones mi
     cliente.user.setStatus("idle")
     console.log(cliente.user.presence.status)
 
-    const testChannel = cliente.channels.cache
-    console.log(testChannel.find( channel =>
-    channel.name === "PRUEBADAW"))  //poner el nombre del servidor
+    // const testChannel = cliente.channels.cache
+    // console.log(testChannel.find( channel =>
+    // channel.name === "PRUEBADAW"))  //poner el nombre del servidor
 
-    console.log(testChannel)
+    // cliente.application.commands.set([
+    //     {
+    //         name: "pongo",
+    //         description: "Pong Ping Pung",
+    //         options: []
+    //     }
+    // ])
+    
 })
+
+cliente.on("messageCreate",async (msg) =>{
+    console.log(msg.author.username)
+    console.log(msg.content)
+    if (msg.author.bot){
+        return
+    }
+
+    if (msg.content === "ping"){
+        await msg.reply("Pong!")
+        await msg.react("")  //Esto sirve para poner emoticonos
+    }
+
+    if (msg.content === "hola"){
+        await msg.channel.send(`Bienvenido ${msg.author}`)
+    }
+
+    if (msg.content === "social"){
+        await msg.channel.send("Este es mi linkedin https://www.linkedin.com/in/luisvib/")
+    }
+
+    let argumentos = msg.content.split(" ")
+    if (argumentos[0] === "Di"){
+        msg.reply(argumentos.splice(1).join(" "))
+    }
+
+    if (msg.content == msg.content.toUpperCase()){
+        msg.channel.send("Relajate EH")
+    }
+
+    if (msg.content.indexOf("ostia") != -1 || msg.content.indexOf("puta") != -1){
+        msg.channel.send("No digas palabrotas JODER")
+    }
+
+})
+
+// cliente.on("interactionCreate",async inter =>{
+//     if (inter.isCommand() && inter.commandName === "pongo"){
+//         await inter.reply("Ping Pang Pung")
+//     }
+// })
 
 cliente.login(DISCORD_TOKEN)
 
